8ab67face4df5fea4923928fe6b5852c8d5aacf7
Jason Donley <jay.donley@gmail.com>
2022-12-06 16:17:17 -0500

    12.6.417

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -13,9 +13,10 @@ class LandingPage extends Component {
                  <i className="tf-ion-play"></i>
              </a>
          </div>
-         <h1>Cincinnati</h1>
-        <h2>Where You Belong</h2>
-         <p>There’s something inviting about the Cincy Region. Here, Midwest friendliness meets Southern charm. This is where a vibrant urban setting gives way to green spaces and waterways. You can sample our world-famous Cincinnati chili or indulge in innovative, buzzworthy local cuisine. Marvel at cutting-edge street art or travel Northern Kentucky's bourbon trail. Discover a region that reveres history yet pursues constant reinvention. Cincy Culture isn't just one thing. But you have to be here to experience it for yourself.</p>
+         <h1 style={{color: 'white', paddingTop: '7%'}}>Cincinnati</h1>
+        <h2 style={{color: 'lightgray'}}>Where You Belong</h2>
+        <div style={{marginTop: '5%', marginLeft: '10%', marginRight: '10%'}}>
+         <p  style={{color: 'white', fontSize: '20px',}}>There’s something inviting about the Cincy Region. Here, Midwest friendliness meets Southern charm. This is where a vibrant urban setting gives way to green spaces and waterways. You can sample our world-famous Cincinnati chili or indulge in innovative, buzzworthy local cuisine. Marvel at cutting-edge street art or travel Northern Kentucky's bourbon trail. Discover a region that reveres history yet pursues constant reinvention. Cincy Culture isn't just one thing. But you have to be here to experience it for yourself.</p></div>
          <a href="#services" class="btn btn-transparent smooth-scroll">Let's Explore</a>
      </div>
  
@@ -27,7 +28,7 @@ class LandingPage extends Component {
 	<div class="container">
 		<div class="row">
 
-			<div class="col-12">
+			<div class="col-12" style={{marginTop: '5%' }}>
 				<div class="title text-center wow fadeIn" data-wow-duration="1500ms">
 					<h2>Why <span class="color">Cincinnati</span> </h2>
 					<div class="border"></div>
@@ -42,7 +43,7 @@ class LandingPage extends Component {
 					</div> 
 					<div class="content text-center">
 						<h3 class="ddd">Our Culture</h3>
-						<p>Cincy is steeped in history, innovation, and creativity. Unearth hidden gems and unique experiences around every corner, from iconic landmarks and Art Deco architecture to renowned art museums, and outdoor murals</p>
+						<p style={{color: 'black', fontSize: '20px',}}>Cincy is steeped in history, innovation, and creativity. Unearth hidden gems and unique experiences around every corner, from iconic landmarks and Art Deco architecture to renowned art museums, and outdoor murals</p>
 					</div>
 				</div>
 			</div>
@@ -53,19 +54,19 @@ class LandingPage extends Component {
 				<div class="block">
 					<div class="content text-center">
 						<h3>Our Attractions</h3>
-						<p>Uncover our history and stroll under the sea at Newport Aquarium with your family, savor a romantic weekend of unique restaurants, museums, and theater. Cincy has it all and is saving you a front row seat. </p>
+						<p style={{color: 'black', fontSize: '20px',}}>Uncover our history and stroll under the sea at Newport Aquarium with your family, savor a romantic weekend of unique restaurants, museums, and theater. Cincy has it all and is saving you a front row seat. </p>
 					</div>
 				</div>
 			</div>
 			</a>
+
+
 			<div class="col-md-12 text-center wow fadeInUp" data-wow-duration="500ms" data-wow-delay="500ms">
 				<div class="block kill-margin-bottom">
-					
-					
 					<a href="#">
 					<div class="content text-center">
 						<h3>Our Sports</h3>
-						<p>From the Bengals of the NFL, the Reds of the MLB, FC Cincinnati of the MLS, our beloved Bearcats, as well as all our other amazing collegiate teams, Cincinnati Sports and Entertainment has it all! And we're waiting for you!</p>
+						<p style={{color: 'black',  fontSize: '20px',}}>From the Bengals of the NFL, the Reds of the MLB, FC Cincinnati of the MLS, our beloved Bearcats, as well as all our other amazing collegiate teams, Cincinnati Sports and Entertainment has it all! And we're waiting for you!</p>
 					</div></a>
 				</div>
 			</div>
@@ -86,8 +87,8 @@ class LandingPage extends Component {
 			</div>
 			<div class="col-lg-6">
 				<div class="content-block">
-					<h2>Diverse & Dynamic</h2>
-					<p>The City of Cincinnati is home to 52 diverse, vibrant neighborhoods, each with its own charm and story. Breathtaking views, historic houses, pastoral-like peacefulness, eccentric arts, and so much more – Cincinnati neighborhoods really do have something for everyone.</p>
+					<h2 style={{color: 'gray'}}>Diverse & Dynamic</h2>
+					<p  style={{color: 'lightgray'}}>The City of Cincinnati is home to 52 diverse, vibrant neighborhoods, each with its own charm and story. Breathtaking views, historic houses, pastoral-like peacefulness, eccentric arts, and so much more – Cincinnati neighborhoods really do have something for everyone.</p>
 					
 					<div class="row">
 						<div class="col-md-6">
@@ -96,8 +97,8 @@ class LandingPage extends Component {
 									
 								</div>
 								<div class="media-body">
-									<h4 class="media-heading">Our History</h4>
-									<p>In 1788, Israel Ludlow, Matthias Denman, and Robert Patterson purchased eight hundred acres from John Cleves Symmes along the Ohio River at the Licking River's mouth. Symmes had purchased two million acres of land from the Confederation Congress in 1787.</p>
+									<h4  style={{color: 'gray'}} class="media-heading">Our History</h4>
+									<p style={{color: 'lightgray'}}>In 1788, Israel Ludlow, Matthias Denman, and Robert Patterson purchased eight hundred acres from John Cleves Symmes along the Ohio River at the Licking River's mouth. Symmes had purchased two million acres of land from the Confederation Congress in 1787.</p>
 								</div>
 							</div>
 						</div>
@@ -107,8 +108,8 @@ class LandingPage extends Component {
 									
 								</div>
 								<div class="media-body">
-									<h4 class="media-heading">Our Future</h4>
-									<p>Cincinnati's diversified economic base includes manufacturing, wholesale and retail trade, insurance and finance, education and health services, government, and transportation. And Ten Fortune 500 companies have established headquarters in Cincinnati.
+									<h4  style={{color: 'gray'}} class="media-heading">Our Future</h4>
+									<p  style={{color: 'lightgray'}}>Cincinnati's diversified economic base includes manufacturing, wholesale and retail trade, insurance and finance, education and health services, government, and transportation. And Ten Fortune 500 companies have established headquarters in Cincinnati.
 									</p>
 								</div>
 							</div>
@@ -127,8 +128,8 @@ class LandingPage extends Component {
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12 text-center">
-				<h2 id="homePageBottomSection">Come and visit. Perhaps stay awhile...</h2>
-				<p id="homeBottomP">Start off your day with a stop on the doughnut trail or brunching on the local favorite sausage and oat dish, goetta. Then, there are rock climbing gyms, Top Golf, go-karting tracks, and public parks with biking and hiking trails—endless ways to fill your day with a uniquely Cincy excursion. In the evening, dine at one of our renowned restaurants, or sample locally brewed beer and locally distilled bourbon and spirits. 
+				<h2 style={{color: 'lightgray'}} id="homePageBottomSection">Come and visit. Perhaps stay awhile...</h2>
+				<p style={{color: 'white'}} id="homeBottomP">Start off your day with a stop on the doughnut trail or brunching on the local favorite sausage and oat dish, goetta. Then, there are rock climbing gyms, Top Golf, go-karting tracks, and public parks with biking and hiking trails—endless ways to fill your day with a uniquely Cincy excursion. In the evening, dine at one of our renowned restaurants, or sample locally brewed beer and locally distilled bourbon and spirits. 
 
 					Whether you’re traveling with little ones or big kids–you’re sure to create unforgettable family memories in Cincy.</p>
 				<a href="contact.html" class="btn btn-main">Take a tour with us</a>
@@ -137,7 +138,7 @@ class LandingPage extends Component {
 	</div>
 </section>
 
-
+<section style={{backgroundImage: 'url("https://images.unsplash.com/photo-1505319883811-74463eb8c32b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80")', backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', width: '100vw', height: '50px'}}>
 <footer id="footer" class="bg-one">
 	<div class="container">
 		<div class="row wow fadeInUp" data-wow-duration="500ms">
@@ -146,7 +147,7 @@ class LandingPage extends Component {
 				
 				<div class="social-icon">
 					<ul class="list-inline">
-						<li class="list-inline-item"><a href="https://www.facebook.com/places/108303715857333/" target="_blank" ><i class="tf-ion-social-facebook"></i></a></li>
+						<li class="list-inline-item"><a href="https://www.facebook.com/places/108303715857333/" target="_blank" ><i class="fa-brands fa-facebook"></i></a></li>
 						<li class="list-inline-item"><a href="https://twitter.com/search?lang=en&q=%23cincinnati" target="_blank" ><i class="tf-ion-social-twitter"></i></a></li>
 						<li class="list-inline-item"><a href="https://youtu.be/nm826jQsE0Q" target="_blank" ><i class="tf-ion-social-youtube"></i></a></li>
 						<li class="list-inline-item"><a href="https://dribbble.com/tags/cincinnati" target="_blank" ><i class="tf-ion-social-dribbble-outline"></i></a></li>
@@ -154,13 +155,14 @@ class LandingPage extends Component {
 					</ul>
 				</div>
 				
-
+               
 				
 
 			</div> 
 		</div> 
 	</div> 
 </footer> 
+</section>
 
 
 
